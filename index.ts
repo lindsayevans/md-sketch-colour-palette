24bd3a86d811ef982e26e9e23fc0af6cee8ec2da
Lindsay Evans <lindsay.evans@boomworks.com.au>
2020-05-30 09:53:54 +1000

    feat: create palette & chip symbols

--- index.ts
+++ index.ts
@@ -7,6 +7,9 @@ import {
   Text,
 } from 'sketch-constructor';
 
+import { titleCase } from 'title-case';
+import * as contrast from 'contrast';
+
 import { AndroidXmlParser } from './AndroidXmlParser';
 
 const parser = new AndroidXmlParser();
@@ -16,80 +19,44 @@ const parser = new AndroidXmlParser();
 
   const newSketch = new Sketch();
 
-  // Create layer fill styles
-  const layerStyle = SharedStyle.LayerStyle({
-    name: '__TMP',
-    fills: [
-      {
-        color: '#007EB6',
-      },
-    ],
-  });
-  newSketch.addLayerStyle(layerStyle);
-
-  // Create symbols
-  const symbolsPage = new Page({
-    name: 'Symbols',
-  });
-  newSketch.addPage(symbolsPage);
-
-  let chipSym = new SymbolMaster({
-    name: 'MD Colour',
-    frame: { width: 200, height: 50 },
-  });
-  let r = new Rectangle({
-    name: 'Background',
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 50,
-  });
-  r.setSharedStyle(layerStyle);
-  chipSym.addLayer(r);
-
-  // TODO: Contrasting text colours (via text styles?)
-  const text = new Text({
-    name: 'ID',
-    string: 'A700',
-    color: '#fff',
-    fontSize: 18,
-    frame: { width: 50, height: 24, x: 16, y: 13 },
-  });
-  chipSym.addLayer(text);
-
-  const hex = new Text({
-    name: 'Hex',
-    string: '#000000',
-    color: '#fff',
-    fontSize: 16,
-    frame: { width: 42, height: 24, x: 58, y: 14 },
-  });
-  chipSym.addLayer(hex);
-
-  chipSym.allowsOverrides = true;
-  symbolsPage.addArtboard(chipSym);
-
   // Create example chips
 
-  const examplesPage = new Page({
-    name: 'Examples',
+  const page = new Page({
+    name: 'Colour Chips',
   });
 
-  newSketch.addPage(examplesPage);
+  newSketch.addPage(page);
 
   palettes.forEach((p, i) => {
+    // Palette symbol
+    const headingHeight = 32;
+    const paletteHeight = 50 * p.colours.length + headingHeight;
+    let pSym = new SymbolMaster({
+      name: `Colours/${titleCase(p.name)}`,
+      frame: {
+        width: 200,
+        height: paletteHeight,
+        x: i * 300,
+        y: 0,
+      },
+    });
+
     const pText = new Text({
       name: 'Palette Name',
-      string: p.name,
+      string: titleCase(p.name),
       color: '#666',
       fontSize: 20,
-      frame: { width: 200, height: 24, x: i * 300, y: 0 },
+      frame: { width: 200, height: headingHeight, x: 0, y: 0 },
     });
-    examplesPage.addLayer(pText);
+    pSym.addLayer(pText);
+
+    pSym.allowsOverrides = false;
+    page.addArtboard(pSym);
 
     p.colours.forEach((c, ii) => {
+      // Fill style
       const layerStyle = SharedStyle.LayerStyle({
-        name: `Colours/${p.name}/${c.id}`,
+        name: `Colours/${titleCase(p.name)}/${c.id}`,
         fills: [
           {
             color: c.hex,
@@ -98,31 +65,57 @@ const parser = new AndroidXmlParser();
       });
       newSketch.addLayerStyle(layerStyle);
 
-      let chipInstance = chipSym.createInstance({
-        name: `Colour Chips/${p.name}/${c.id}`,
-        frame: { width: 200, height: 50, x: i * 300, y: 24 + ii * 50 },
-        overrideValues: [
-          {
-            name: 'ID',
-            value: c.id,
-          },
-          {
-            name: 'Hex',
-            value: c.hex,
-          },
-          {
-            name: 'Background',
-            value: layerStyle,
-          },
-        ],
+      // Colour chip symbol
+      let chipSym = new SymbolMaster({
+        name: `Colours/${titleCase(p.name)}/${c.id}`,
+        frame: {
+          width: 200,
+          height: 50,
+          x: i * 300,
+          y: paletteHeight + (headingHeight + ii * 100),
+        },
       });
+      let r = new Rectangle({
+        name: 'Background',
+        x: 0,
+        y: 0,
+        width: 200,
+        height: 50,
+      });
+      r.setSharedStyle(layerStyle);
+      chipSym.addLayer(r);
 
-      // Workaround for bug in sketch-constructor that frame position isn't picked up from createInstance()
-      chipInstance.frame.x = i * 300;
-      chipInstance.frame.y = 24 + ii * 50;
+      const textColour = contrast(c.hex) === 'light' ? '#1a1a1a' : '#f4f4f4';
 
-      examplesPage.addLayer(chipInstance);
-      // TODO: Add as symbols - palette, chips
+      const text = new Text({
+        name: 'ID',
+        string: c.id,
+        color: textColour,
+        fontName: 'Helvetica',
+        fontSize: 24,
+        verticalAlignment: 'center',
+        frame: { width: 50, height: 24, x: 16, y: 11 },
+      });
+      chipSym.addLayer(text);
+
+      const hex = new Text({
+        name: 'Hex',
+        string: c.hex,
+        color: textColour,
+        fontName: 'Monaco',
+        fontSize: 16,
+        alignment: 'right',
+        verticalAlignment: 'center',
+        frame: { width: 90, height: 24, x: 100, y: 18 },
+      });
+      chipSym.addLayer(hex);
+
+      chipSym.allowsOverrides = false;
+      page.addArtboard(chipSym);
+
+      const chip = chipSym.createInstance({});
+      chip.frame.x = 0;
+      (chip.frame.y = headingHeight + ii * 50), pSym.addLayer(chip);
     });
   });
 
